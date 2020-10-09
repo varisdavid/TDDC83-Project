9578c7d4490c0f8d0d6117f9075663544e65ac82
Emil Strömberg <emist981@student.liu.se>
2020-10-09 12:14:53 +0200

    Using yarn test to run the testsuite, made the current template test work

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders patient management', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText("Patient management");
   expect(linkElement).toBeInTheDocument();
 });
