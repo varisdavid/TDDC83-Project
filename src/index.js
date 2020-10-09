7cea2b4a68d9d4c2087189d7975f1dd3ad7b744e
Emil Strömberg <emist981@student.liu.se>
2020-10-09 13:13:24 +0200

    updated browserslist

--- src/index.js
+++ src/index.js
@@ -5,10 +5,21 @@ import './styles.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Auth0Provider } from "@auth0/auth0-react";
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <Auth0Provider
+    domain="dev-mlj1m1lm.eu.auth0.com"
+    clientId="hTDLbigJVn6L1UJ2dwcKodjrU3l79dQx"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
