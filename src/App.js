4f937d9df6e0f2e861895e1116441347ad5d2130
Emil Strömberg <emist981@student.liu.se>
2020-10-13 09:06:06 +0200

    added a protected route for patient-overview, as well as a view (PatientOverview.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 
 import { NavBar, Footer, Loading } from "./components";
-import { Home, Profile, ExternalApi } from "./views";
+import { Home, Profile, ExternalApi, PatientOverview } from "./views";
 import ProtectedRoute from "./auth/ProtectedRoute";
 
 import "./app.css";
@@ -23,6 +23,7 @@ const App = () => {
           <Route path="/" exact component={Home} />
           <ProtectedRoute path="/profile" component={Profile} />
           <ProtectedRoute path="/external-api" component={ExternalApi} />
+          <ProtectedRoute path="/overview" component={PatientOverview} /> {/* Just a starting point, could be dashboard, patients etc .. */}
         </Switch>
       </div>
       <Footer />
