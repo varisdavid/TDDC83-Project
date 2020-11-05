d7fdc502698abf468d9820b93e227311e2a4d8ee
David Varis <davfr002@student.liu.se>
2020-11-05 16:39:17 +0100

    Skeleton for backend features implemented

---
+++ Features/Backend/Server/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+#Change to include real path
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#WIP
+#Imports database data
+from server.database import db, Employee, Team, Department
+
+#Imports blueprints
+#from server.blueprints import pers_data ...
+
+#Register defined routes of blueprints
+#app.register_blueprint(pers_data.bp)
