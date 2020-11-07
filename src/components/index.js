e551fa16d9aea3af2268d08d1643e89bfdc42c96
Emil Strömberg <emist981@student.liu.se>
2020-11-07 15:37:03 +0100

    prettified components/index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,26 +1,26 @@
-import HomeContent from "./HomeContent";
-import Footer from "./Footer";
-import Hero from "./Hero";
-import Loading from "./Loading";
-import NavBar from "./NavBar";
-// import PatientCalendar from "./PatientCalendar";
-// import PatientHeader from "./PatientHeader";
-// import NavBarPatient from "./NavBarPatient";
+import HomeContent from './HomeContent';
+import Footer from './Footer';
+import Hero from './Hero';
+import Loading from './Loading';
+import NavBar from './NavBar';
+// import PatientCalendar from './PatientCalendar';
+// import PatientHeader from './PatientHeader';
+// import NavBarPatient from './NavBarPatient';
 
-import OverviewHeaderBanner from "./OverviewHeaderBanner";
-import OverviewHeaderNavigation from "./OverviewHeaderNavigation";
+import OverviewHeaderBanner from './OverviewHeaderBanner';
+import OverviewHeaderNavigation from './OverviewHeaderNavigation';
 
-import OverviewCalendar from "./OverviewCalendar";
+import OverviewCalendar from './OverviewCalendar';
 
-import Patients from "./Patients";
-import PatientsSearch from "./PatientsSearch"
-import PatientsTable from "./PatientsTable"
-import PatientGroups from "./PatientGroups";
+import Patients from './Patients';
+import PatientsSearch from './PatientsSearch'
+import PatientsTable from './PatientsTable'
+import PatientGroups from './PatientGroups';
 
-import FilterModal from "./FilterModal";
+import FilterModal from './FilterModal';
 
-import Notices from "./Notices";
-import NoticesTable from "./NoticesTable";
+import Notices from './Notices';
+import NoticesTable from './NoticesTable';
 
 export { 
     HomeContent, 
