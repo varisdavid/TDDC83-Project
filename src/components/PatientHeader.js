56b049318685ae4925e8e989e210719962cee223
lukev051 <lukev051@student.liu.se>
2020-11-08 15:35:47 +0100

    New branch to change to correct syntax

--- src/components/PatientHeader.js
+++ src/components/PatientHeader.js
@@ -3,7 +3,7 @@ import React from "react";
 const PatientHeader = () => {
     return (
         <div className="text-center mb-3">
-            <h1 className="mb-4">Gunilla Andersson, 471028-xxxx</h1>
+            <h1 className="mb-4">Gunilla Andersson, 471029-xxxx</h1>
         </div>
     );
 };
