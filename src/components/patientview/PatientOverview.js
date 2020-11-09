fe9b06db1909b13b16bed9615770ac33141a49c7
lukev051 <lukev051@student.liu.se>
2020-11-09 22:11:11 +0100

    Tried to fix pipline error

--- src/components/patientview/PatientOverview.js
+++ src/components/patientview/PatientOverview.js
@@ -1,8 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
-
-import { PatientsSearch, PatientsTable, PatientGroups } from '..'
-import { useTable, useFlexLayout, useFilters, useGlobalFilter, useSortBy } from 'react-table'
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
+import React from 'react';
 
 const PatientOverview = () => {
 
