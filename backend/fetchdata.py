643ff3d92d9f68b5269fb96b81732c359b24566b
Jonathan Vikbladh <jonvi252@student.liu.se>
2020-11-09 22:08:31 +0100

    trying a first simple endpoint, almost done

--- backend/fetchdata.py
+++ backend/fetchdata.py
@@ -13,12 +13,38 @@ def query(aql):
                             )
     return response.json() if response.ok else response
 
-
 def getParty(ehrid):
     response = requests.get(baseurl +"/demographics/party/query?ehrId=" + ehrid,
                             verify=True,
                             auth=(wu,wp))
     return response.json() if response.ok else response
+def get_all_patients_personal_details():
+    """
+    Function that retrieves the personal details for each of our patients in the ehrScape database
+    Parameters: None
+    Returns: 
+    """
+    aql_ehrids = """SELECT e/ehr_id/value as id
+                    FROM EHR e
+                    CONTAINS COMPOSITION c[openEHR-EHR-COMPOSITION.encounter.v1]
+                    WHERE c/name/value='EHR-PUM-C3'
+                    OFFSET 0"""
+    response = query(aql_ehrids)
+    to_return = []
+    list_of_dicts_with_ehrids = response['resultSet']
+    for ehrid_dict in list_of_dicts_with_ehrids:
+        ehrid = ehrid_dict['id']
+        party = getParty(ehrid)
+        names = party['parties'][0]
+        more_info = names['additionalInfo']
+        personal_details = {"Förnamn": names['firstNames'], "Efternamn" : names['lastNames'],
+                            "ehrid" : more_info['ehrId'], "personnummer" : more_info['pnummer'],
+                            "Kön" : more_info['gender'], "Stad" :more_info['city'],
+                            "Address" : more_info['adress'], "Ålder" : more_info['age'],
+                            "Telefon" : more_info['phone'], "Email" : more_info['email'] }
+        to_return.append(personal_details)
+    print(to_return)
+get_all_patients_personal_details()
 
 
 
