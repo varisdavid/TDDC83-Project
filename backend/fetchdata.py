676d9341871ab10ba22daf1c06f8f20054700dea
Jonathan Vikbladh <jonvi252@student.liu.se>
2020-11-09 22:05:37 +0100

    wrote function that can handle whatever AQL query, posts it to the REST api

--- backend/fetchdata.py
+++ backend/fetchdata.py
@@ -6,3 +6,9 @@ wp = "lio.se123"
 
 baseurl = 'https://rest.ehrscape.com/rest/v1'
 
+def query(aql):
+    response = requests.get(baseurl+'/query/?aql=' + aql,
+                            verify= True,
+                            auth = (wu,wp)
+                            )
+    return response.json() if response.ok else response
