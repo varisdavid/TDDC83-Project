e4aacc74386f76e3ddb61dea9d20756adef1d8c9
Jonathan Vikbladh <jonvi252@student.liu.se>
2020-11-09 21:42:06 +0100

    problems with querying AQL via REST

--- backend/deleteMockPatients.py
+++ backend/deleteMockPatients.py
@@ -12,9 +12,16 @@ wp = os.environ.get("EHRSCAPE_PASSWORD")
 #baseurl for all calls to the ehrscape REST-api
 baseurl = 'https://rest.ehrscape.com/rest/v1'
 
+
+aql_get_mock_patient_ehrids = """SELECT e/ehr_id/value as id
+FROM EHR e
+CONTAINS COMPOSITION c[openEHR-EHR-COMPOSITION.encounter.v1]
+WHERE c/name/value='EHR-PUM-C3'
+OFFSET 0"""  
 def query(aql):
     response = requests.get(baseurl+'/query/', params=aql
                             verify= True,
                             auth = (wu,wp)
                             )
     return response.json() if response.ok else response
+query(aql_get_mock_patient_ehrids)
