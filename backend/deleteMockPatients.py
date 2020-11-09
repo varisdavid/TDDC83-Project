513c318310badd49a82ececacd6394e49fc658f7
Jonathan Vikbladh <jonvi252@student.liu.se>
2020-11-09 21:43:31 +0100

    calling REST with AQL query now ok

--- backend/deleteMockPatients.py
+++ backend/deleteMockPatients.py
@@ -19,7 +19,7 @@ CONTAINS COMPOSITION c[openEHR-EHR-COMPOSITION.encounter.v1]
 WHERE c/name/value='EHR-PUM-C3'
 OFFSET 0"""  
 def query(aql):
-    response = requests.get(baseurl+'/query/', params=aql
+    response = requests.get(baseurl+/'query/?aql=' + aql,
                             verify= True,
                             auth = (wu,wp)
                             )
