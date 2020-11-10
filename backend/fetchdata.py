ad44c61829af9f23334b1f3e8edecf6d741d774f
Emil Strömberg <emist981@student.liu.se>
2020-11-10 11:19:46 +0100

    Added a flask wrapper to endpoint for fetching list of patients

--- backend/fetchdata.py
+++ backend/fetchdata.py
@@ -1,23 +1,35 @@
+from flask import Flask, request, redirect, url_for, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/patientlist", methods=["GET"])
+def patient_list():
+    patient_list = get_all_patients_personal_details()
+    return jsonify(patient_list)
+
+
 import requests
 import json
 
 wu = "lio.se1"
 wp = "lio.se123"
 
-baseurl = 'https://rest.ehrscape.com/rest/v1'
+baseurl = "https://rest.ehrscape.com/rest/v1"
+
 
 def query(aql):
-    response = requests.get(baseurl+'/query/?aql=' + aql,
-                            verify= True,
-                            auth = (wu,wp)
-                            )
+    response = requests.get(baseurl + "/query/?aql=" + aql, verify=True, auth=(wu, wp))
     return response.json() if response.ok else response
 
+
 def getParty(ehrid):
-    response = requests.get(baseurl +"/demographics/party/query?ehrId=" + ehrid,
-                            verify=True,
-                            auth=(wu,wp))
+    response = requests.get(
+        baseurl + "/demographics/party/query?ehrId=" + ehrid, verify=True, auth=(wu, wp)
+    )
     return response.json() if response.ok else response
+
+
 def get_all_patients_personal_details():
     """
     Function that retrieves the personal details for each of our patients in the ehrScape database
@@ -31,17 +43,26 @@ def get_all_patients_personal_details():
                     OFFSET 0"""
     response = query(aql_ehrids)
     to_return = []
-    list_of_dicts_with_ehrids = response['resultSet']
+    list_of_dicts_with_ehrids = response["resultSet"]
     for ehrid_dict in list_of_dicts_with_ehrids:
-        ehrid = ehrid_dict['id']
+        ehrid = ehrid_dict["id"]
         party = getParty(ehrid)
-        names = party['parties'][0]
-        more_info = names['additionalInfo']
-        personal_details = {"Förnamn": names['firstNames'], "Efternamn" : names['lastNames'],
-                            "ehrid" : more_info['ehrId'], "personnummer" : more_info['pnummer'],
-                            "Kön" : more_info['gender'], "Stad" :more_info['city'],
-                            "Address" : more_info['adress'], "Ålder" : more_info['age'],
-                            "Telefon" : more_info['phone'], "Email" : more_info['email'] }
+        names = party["parties"][0]
+        more_info = names["additionalInfo"]
+        personal_details = {
+            "Name": names["firstNames"] + " " + names["lastNames"],
+            "EhrID": more_info["ehrId"],
+            "PNR": more_info["pnummer"],
+            "Gender": more_info["gender"],
+            "City": more_info["city"],
+            "Address": more_info["adress"],
+            "Age": more_info["age"],
+            "Phone": more_info["phone"],
+            "Email": more_info["email"],
+        }
         to_return.append(personal_details)
     return to_return
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
