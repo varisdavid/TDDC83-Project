d6e53a0037e3d816347d9f6d7080950089e3d4e3
lukev051 <lukev051@student.liu.se>
2020-11-10 23:02:45 +0100

    Tables for patient/overview is almost done

--- src/components/patientview/PatientOverview.js
+++ src/components/patientview/PatientOverview.js
@@ -1,96 +1,181 @@
 import React, { useMemo } from 'react';
 
-import { PatientTables } from '..'
-
-import { useTable, useFlexLayout} from 'react-table'
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
 
 const PatientOverview = () => {
 
-    const data = useMemo(
-          () => [
-            {
-              priority: 1,
-              name: "Gunilla Andersson",
-              sweID: '470203-1324',
-              diagnoses: ['Diabetes', 'Hypertoni'],
-              updatedAt: '2020-10-08',
-              updatedBy: 'Patienten',
-              age: 73,
-              gender: 'female',
-              team: 'Team 1',
-              department: 'Department 1'
-            },
-          ],
-          []
-    )
-
-    const data2 = useMemo(
-        () => [
-          {
-            priority: 1,
-            name: "Gunilla Andersson",
-            sweID: '470203-1324',
-            diagnoses: ['Diabetes', 'Hypertoni'],
-            updatedAt: '2020-10-08',
-            updatedBy: 'Patienten',
-            age: 73,
-            gender: 'female',
-            team: 'Team 1',
-            department: 'Department 1'
-          },
-        ],
-        []
-  )
-    
-    const columns = useMemo (
-          () => [
-            {
-                Header: 'Rubrik',
-                accessor: 'info',
-            },
-          ],
-          []
-    )
-
-    const columns2 = useMemo (
-        () => [
-          {
-              Header: 'Inte rubrik',
-              accessor: 'info',
-          },
-        ],
-        []
+  const data = useMemo(
+    () => [
+      {
+        priority: 1,
+        name: "Gunilla Andersson",
+        sweID: '470203-1324',
+        diagnoses: ['Diabetes', 'Hypertoni'],
+        updatedAt: '2020-10-08',
+        updatedBy: 'Patienten',
+        age: 73,
+        gender: 'female',
+        team: 'Team 1',
+        department: 'Department 1',
+        length: 164,
+        weight: 60,
+        phone: '070-9292929',
+        email: "gunilla@andersson.se",
+        address: "Linköpingsvägen 1",
+        emergencyContact: "Per Andersson",
+        emergencyContactPhone: '070-9292929',
+        emergencyContactEmail: "per@andersson.se",
+        emergencyContactAddress: "Linköpingsvägen 1"
+      },
+    ],
+    []
   )
 
-    // Creates an instance of table, given columns, data and an initial state.
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable({columns, data}, //Columns?
-        useFlexLayout,
-    );
+  // One after another the tables for Förnamn Efternamn, Kontaktinformation, Nödkontakt, Nuvarnade Diagnoser...
+  return (
+    <div className='flex justify-center'>
+        <div style={{ width: '25%' }}>        
+          <div style={{ width: '80%', marginBottom: '30px'}}>
+          {data.map((data) => (
+            <Table className='table-patient'>
+              <TableHead>
+                <TableRow>
+                  <TableCell className='cell-table-header'>
+                    {data.name}
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell className='cell-table-body' style={{background:'#FFF'}}> Kön: {data.gender} </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='cell-table-body'> Ålder: {data.age} </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='cell-table-body' style={{background:'#FFF'}}> Längd (cm): {data.length} </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='cell-table-body'> Vikt (kg): {data.weight} </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          ))}
+          </div>
 
-    return (
-        <div className='flex justify-center'>
-            <div style={{ width: 'calc(85%)', marginRight: '22.5px' }} className='mt-3 p-2'>
-                <div style={{ width: 'calc(100%)' }}>
-                    <PatientTables
-                        data={data2}
-                        columns={columns2}
-                        getTableProps={getTableProps}
-                        getTableBodyProps={getTableBodyProps}
-                        headerGroups={headerGroups}
-                        rows={rows}
-                        prepareRow={prepareRow}
-                    />
-                </div>
-            </div>
+          <div style={{ width: '80%' }}>
+          {data.map((data) => (
+            <Table className='table-patient'>
+              <TableHead>
+                <TableRow>
+                  <TableCell className='cell-table-header'>
+                    Kontaktinformation
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell className='cell-table-body' style={{background:'#FFF'}}> Telefon: {data.phone} </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='cell-table-body'> Mejladress: {data.email} </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='cell-table-body' style={{background:'#FFF'}}> Adress: {data.address} </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          ))}
+          </div>
+
+          <div style={{ width: '80%' }} className='vertical-align' >
+          {data.map((data) => (
+            <Table className='table-patient'>
+              <TableHead>
+                <TableRow>
+                  <TableCell className='cell-table-header'>
+                    Nödkontakt
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell className='cell-table-body' style={{background:'#FFF'}}> Kön: {data.gender} </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='cell-table-body'> Ålder: {data.age} </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell className='cell-table-body' style={{background:'#FFF'}}> Längd (cm): {data.length} </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          ))}
+          </div>
+
         </div>
 
-    );
+        <div style={{ width: '25%' }} className='vertical-align'>
+          <div style={{ width: '80%' }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell className='cell-table-header'>
+                    {data.name}
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    Text
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+          <div style={{ width: '80%' }} className='vertical-align'>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell className='cell-table-header'>
+                    {data.name}
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    Text
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+
+        <div style={{ width: '50%' }}>
+          <div style={{ width: '80%' }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell className='cell-table-header'>
+                    {data.name}
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                      Text
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+    </div>
+  );
 };
 
 export default PatientOverview;
