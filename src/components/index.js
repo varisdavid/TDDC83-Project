c02320f1a72d561d4a6e6cff3129d33bfbc39821
Gustaf Eriksson <guser692@student.liu.se>
2020-11-12 09:01:32 +0100

    Created PatientListOfMedication

--- src/components/index.js
+++ src/components/index.js
@@ -23,6 +23,7 @@ import CreateNewFilterModal from './overview/CreateNewFilterModal';
 import PatientCalendar from './PatientCalendar';
 import PatientViewHeaderBanner from './PatientViewHeaderBanner';
 import PatientViewHeaderNavigation from './PatientViewHeaderNavigation';
+import PatientListOfMedication from './PatientListOfMedication';
 
 
 // import Notices from './Notices';
@@ -46,4 +47,5 @@ export {
     PatientCalendar,
     PatientViewHeaderBanner,
     PatientViewHeaderNavigation,
+    PatientListOfMedication,
 };
