905fb6d8d862ef0a64d9da0f253f8a24779f9be1
Jonathan Vikbladh <jonvi252@student.liu.se>
2020-11-13 16:52:42 +0100

    added endpoint for fetching measurements data for a single patient

--- backend/patient_list/fetch_patient_list.py
+++ backend/patient_list/fetch_patient_list.py
@@ -48,10 +48,47 @@ def get_all_patients_personal_details():
             "Age": more_info["age"],
             "Phone": more_info["phone"],
             "Email": more_info["email"],
-            "Team" : more_info["team"],
-            "Department" : more_info["department"],
-            "Contactperson" : more_info["contactperson"]
+            "Team" : more_info["team"], #is now a list so it can contain more than 1 team
+            "Department" : more_info["department"], #for now department is still string, but will be made into list like team soon
+            "Contactperson" : more_info["contactperson"] #Dict containing Name (string on form "Firstname Lastname" and phonenummer)
         }
         to_return.append(personal_details)
     return to_return
+
+#Second endpoint
+def get_measurements(ehrid):
+    """
+    Function that retrieves measurements data for a patient
+    Parameters: ehrId, identifier for a single patient
+    """
+
+    aql = """SELECT x/data[at0002]/events[at0003]/data[at0001]/items[at0004,'Pulse Rate']/value as pulse,
+       a/data[at0001]/events[at0002]/data[at0003]/items[at0011]/value as exercise,
+       o/data[at0002]/events[at0003]/data[at0001]/items[at0004]/value as bodyweight,
+       w as bloodsugar,
+       i/data[at0001]/events[at0006]/data[at0003]/items[at0004]/value as systolic,
+       i/data[at0001]/events[at0006]/data[at0003]/items[at0005]/value as diastolic
+       FROM EHR e
+       CONTAINS COMPOSITION c
+       CONTAINS (OBSERVATION x[openEHR-EHR-OBSERVATION.pulse.v1] and 
+       OBSERVATION a[openEHR-EHR-OBSERVATION.physicalactivityrecord.v0] and 
+       OBSERVATION o[openEHR-EHR-OBSERVATION.body_weight.v2] and 
+       OBSERVATION w[openEHR-EHR-OBSERVATION.blood_glucose.v1] and 
+       OBSERVATION i[openEHR-EHR-OBSERVATION.blood_pressure.v2]) 
+       WHERE e/ehr_id/value= '%s'
+       OFFSET 0""" %ehrid
+    response = query(aql)
+    to_return= []
+    for measurement in response['resultSet']:
+        to_return.append({"Pulse: " : measurement['pulse']['magnitude'],
+                          "Exercise: ":measurement['exercise']['magnitude'],
+                          "Weight: " : measurement['bodyweight']['magnitude'],
+                          "Diastolic: " : measurement['diastolic']['magnitude'],
+                          "Systolic: " : measurement['systolic']['magnitude'],
+                          "Time: " : measurement['bloodsugar']['data']['origin']['value'], #need to reformat time-strings, currently in format "2020-11-13T12:46:36+01:00" for example
+                          "Bloodsugar: " : measurement['bloodsugar']['data']['events'][0]['data']['items'][0]['value']['magnitude']})
+    return to_return
+
+
+    
 
