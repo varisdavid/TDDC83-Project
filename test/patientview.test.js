32c5df029c9565bc2300f13a2f3d4f5d41f8a934
Gustav Karlsson <gustavkarlsson@Gustavs-MacBook-Pro.local>
2020-11-13 15:51:37 +0100

    Fungerande test av filtrering

--- test/patientview.test.js
+++ test/patientview.test.js
@@ -33,15 +33,17 @@ it('should try to access patient data but press cancel and still be at overview 
 })
 it('should filter all different filters on the current patients in our database FR53', async () =>{
   let anchor = await querySelector('/html/body/div/div/div[3]/div/div/div[1]/div/div/div[2]/button[1]/span[1]', driver);
-  const filter = await driver.findElement(By.xpath('/html/body/div/div/div[3]/div/div/div[1]/div/div/div[2]/button[1]/span[1]'));
-  await filter.click();
+  const filter = '/html/body/div/div/div[3]/div/div/div[1]/div/div/div[2]/button[1]/span[1]';
+  await driver.findElement(By.xpath(filter)).click();
   let e = await querySelector('/html/body/div[2]/div[3]/div/div[2]/div[1]/div[1]/div', driver);
-  const gender = await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/div[1]/div[1]/div'));
+  const gender = '/html/body/div[2]/div[3]/div/div[2]/div[1]/div[1]/div'
   
-  const team = await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/div[1]/div[2]/div'));
-  const depart = await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/div[1]/div[3]/div'));
+  const team = '/html/body/div[2]/div[3]/div/div[2]/div[1]/div[2]/div'
+  const depart = '/html/body/div[2]/div[3]/div/div[2]/div[1]/div[3]/div'
 
   var clickArray = ['/html/body/div[3]/div[3]/ul/li[1]','/html/body/div[3]/div[3]/ul/li[2]', '/html/body/div[3]/div[3]/ul/li[3]']
+  var teamArray = ['/html/body/div[3]/div[3]/ul/li[1]','/html/body/div[3]/div[3]/ul/li[2]', '/html/body/div[3]/div[3]/ul/li[3]']
+  var departArray = ['/html/body/div[3]/div[3]/ul/li[1]','/html/body/div[3]/div[3]/ul/li[2]', '/html/body/div[3]/div[3]/ul/li[3]']
   var priorityArray = ['/html/body/div[2]/div[3]/div/div[2]/div[1]/fieldset/div/label[1]/span[1]/span[1]/input', '/html/body/div[2]/div[3]/div/div[2]/div[1]/fieldset/div/label[2]/span[1]/span[1]/input', 
 '/html/body/div[2]/div[3]/div/div[2]/div[1]/fieldset/div/label[3]/span[1]/span[1]/input']
   await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/button/span[1]')).click();
@@ -49,23 +51,28 @@ it('should filter all different filters on the current patients in our database 
   var i, j, k, x
   
 for (i = 0; i < clickArray.length; i++) {
-    for (j = 0; j < clickArray.length; j++) {
+    for (j = 0; j < teamArray.length; j++) {
     
-      for (k = 0; k < clickArray.length; k++){
+      for (k = 0; k < departArray.length; k++){
         
-          for (x = 0; x < priorityArray.length; i++) {
-            await filter.click();
-            let j = await querySelector('/html/body/div[2]/div[3]/div/div[2]/div[1]/div[1]/div', driver);
-            gender.click();
+          for (x = 0; x < priorityArray.length; x++) {
+            await driver.findElement(By.xpath('/html/body/div/div/div[3]/div/div/div[1]/div/div/div[2]/button[1]/span[1]'));
+            await driver.findElement(By.xpath(filter)).click();
+            let z = await querySelector('/html/body/div[2]/div[3]/div/div[2]/div[1]/div[1]/div', driver);
+            await new Promise(r => setTimeout(r, 200));
+            await driver.findElement(By.xpath(gender)).click();
             await driver.findElement(By.xpath(clickArray[i])).click();
-            team.click();
-            await driver.findElement(By.xpath(clickArray[j])).click();
-             depart.click();
-            await driver.findElement(By.xpath(clickArray[k])).click();
-        
+            await new Promise(r => setTimeout(r, 200));
+            await driver.findElement(By.xpath(team)).click();
+            
+            await driver.findElement(By.xpath(teamArray[j])).click();
+            await new Promise(r => setTimeout(r, 200));
+            await driver.findElement(By.xpath(depart)).click();
+            await driver.findElement(By.xpath(departArray[k])).click();
+            await new Promise(r => setTimeout(r, 500));
             await driver.findElement(By.xpath(priorityArray[x])).click();
             await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/button/span[1]')).click();
-            driver.navigate().refresh();
+            await driver.findElement(By.xpath('/html/body/div/div/div[3]/div/div/div[1]/div/div/div[2]/button[1]')).click();
 
 
         } 
