f8bc4c1bbddc0274c62fd6c13b8398ef51162439
Emil Strömberg <emist981@student.liu.se>
2020-11-16 14:39:31 +0100

    Deep integration of current backend, proposed structure.

---
+++ backend/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from .config import Config
+
+db = SQLAlchemy()
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_object("backend.config.Config")
+
+    db.init_app(app)
+
+    from .blueprints.access_log_bp import access_log_bp
+    from .blueprints.patient_list_bp import patient_list_bp
+
+    # from .blueprints.employee_bp import employee_bp
+
+    app.register_blueprint(patient_list_bp)
+    app.register_blueprint(access_log_bp)
+
+    # app.register_blueprint(employee_bp)
+
+    from .cli_commands import cli
+
+    app.cli.add_command(cli)
+
+    return app
+
