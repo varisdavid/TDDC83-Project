d9e36eb5cdc428a9fcc32ea64d991c99a7e83c21
Gustav Karlsson <gustavkarlsson@Gustavs-MacBook-Pro.local>
2020-11-17 18:20:19 +0100

    Removed application from testingBranch, to reduce redundancy

--- src/components/common/Label.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Label = ({ text }) => (
-  <label className="text-sm font-semibold text-gray-700">{text}</label>
-);
-
-export default Label;
--- src/views/index.js
+++
@@ -1,9 +0,0 @@
-import ExternalApi from "./ExternalApi";
-import Home from "./Home";
-import Profile from "./Profile";
-import PatientView from "./PatientView"
-import Overview from "./Overview";
-import OverviewSettings from "./OverviewSettings";
-
-
-export { ExternalApi, Home, Profile, Overview, OverviewSettings, PatientView };
