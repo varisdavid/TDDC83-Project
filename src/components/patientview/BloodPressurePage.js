78bee39782f57ff9d06e6996fec7a1e0c8311484
Gustaf Eriksson <guser692@student.liu.se>
2020-11-20 12:12:07 +0100

    Fixed styling so the update information square looks like the hi-fi prototype

--- src/components/patientview/BloodPressurePage.js
+++ src/components/patientview/BloodPressurePage.js
@@ -187,32 +187,52 @@ const BloodPressurePage = () => {
                     </Table>
 
                     <form style={{
-                        border: '1px solid black',
+                        border: '1px solid lightgrey',
                         marginLeft: "auto",
                         marginRight: "auto",
                         marginTop: "1.5rem",
                         boxShadow: "5px 7px 20px lightgrey",
 
                     }}>
-                        <h1 style={{marginLeft: "10px", marginRight: "10px", marginTop: "10px", fontWeight: "500"}}>Uppdatera
+                        <h1 style={{
+                            marginLeft: "10px",
+                            marginRight: "10px",
+                            marginTop: "10px",
+                            fontWeight: "500"
+                        }}>Uppdatera
                             information</h1>
                         <br/>
-                        <text style={{marginLeft: "10px", marginRight: "10px", marginTop: "10px"}}>Datum:</text>
-                        <input style={{border: '1px solid black', marginLeft: "auto", marginRight: "auto"}}
-                               name="date"/>
+                        <text style={{
+                            marginLeft: "10px",
+                            marginRight: "10px",
+                            marginTop: "10px"
+                        }}>Datum:</text>
+                        <input style={{
+                            border: '1px solid lightgrey',
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                            boxShadow: "inset 0 2px 3px lightgrey"
+                        }} name="date"/>
                         <br/>
-                        <text style={{marginLeft: "10px", marginRight: "10px", marginTop: "10px"}}>Fyll i ny uppmätt
+                        <text style={{
+                            marginLeft: "10px",
+                            marginRight: "10px",
+                            marginTop: "10px"
+                        }}>Fyll i ny uppmätt
                             vikt:
                         </text>
                         <input style={{
-                            border: '1px solid black',
+                            border: '1px solid lightgrey',
                             marginLeft: "auto",
-                            marginRight: "auto",
+                            marginRight: "20px",
                             marginTop: "1.5rem",
-
+                            boxShadow: "inset 0 2px 3px lightgrey",
                         }} name="BloodPressureMeasurement"/>
                         <br/>
-                        <div align='right' style={{marginRight:"10px", marginBottom:"10px"}}>
+                        <div align='right' style={{
+                            marginRight:"10px",
+                            marginBottom:"10px"
+                        }}>
                             <Button
                                 className='flex shadow'
                                 style={{
@@ -222,8 +242,7 @@ const BloodPressurePage = () => {
                                     marginLeft: "auto",
                                     marginRight: "auto",
                                     marginTop: "1.5rem",
-                                }}
-                            >
+                                }}>
                                 Bekräfta
                             </Button>
                         </div>
