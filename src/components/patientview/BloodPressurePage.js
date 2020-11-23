32187659829d8abd88c08bdfff251db8cfd4cc62
Gustaf Eriksson <guser692@student.liu.se>
2020-11-23 18:58:27 +0100

    Added new modals and funtions to the Warning Notification in BloodPressurePage.js this can be used in the WeightChartPage.js as well as PhysicalActivityPage.js

--- src/components/patientview/BloodPressurePage.js
+++ src/components/patientview/BloodPressurePage.js
@@ -30,29 +30,49 @@ const BloodPressurePage = () => {
         const href= "";
         const history = useHistory();
 
-        // Keeps track of whether or not the popup for navigating to specific patient view has been toggled.
-        const [openPatientViewConfirm, setOpenPatientViewConfirm] = useState(false);
+        // Keeps track of whether or not the popup for a specific alert  has been toggled.
+        const [openWarningConfirm, setOpenWarningConfirm] = useState(false);
+        const [openWarningConfirm2, setOpenWarningConfirm2] = useState(false);
+        const [openWarningConfirm3, setOpenWarningConfirm3] = useState(false);
 
         // Handles opening of modal window
         const handleOpenConfirmation = () => {
-            setOpenPatientViewConfirm(true);
+            setOpenWarningConfirm(true);
         };
 
         // Handles closing of modal window
         const handleCloseConfirmation = () => {
-            setOpenPatientViewConfirm(false); // Close modal
+            setOpenWarningConfirm(false); // Close modal
         };
-
-        const navigateToPatientView = () => {
-            history.push(href);
-            setOpenPatientViewConfirm(false); // Close modal
+        //Handels closing of modal no.2 window
+        const handleCloseConfirmation2 = () => {
+            setOpenWarningConfirm2(false); // Close modal
+        };
+        //Handels closing of modal no.3 window
+        const handleCloseConfirmation3 = () => {
+            setOpenWarningConfirm3(false); // Close modal
+        };
+        //Closes the first modal and opens the second one
+        const OpenWarning2 = () => {
+            setOpenWarningConfirm2( true); //Open modal no. 2
+            setOpenWarningConfirm(false); // Close modal
+        };
+        //Sends us to the PatientView Overview
+        const sendToPatient = () => {
+            history.push(href); //Should be switched to patientview overview refrens
+            setOpenWarningConfirm2 (false); // Close modal no.2
+        };
+        //Closes the second modal and opens the third
+        const OpenWarning3 = () => {
+            setOpenWarningConfirm3 (true);
+            setOpenWarningConfirm2 (false);
         };
 
         const ConfirmWarning = () => {
 
             return (
                 <Modal
-                    open={openPatientViewConfirm}
+                    open={openWarningConfirm}
                     onClose={handleCloseConfirmation}
                     aria-labelledby='modal-popup'
                 >
@@ -74,7 +94,7 @@ const BloodPressurePage = () => {
                             <Button
                                 className='flex shadow'
                                 style={{ border: '2px solid #0066B3', borderRadius: "0px", width: '110px', marginLeft: "auto", marginRight: "auto", marginTop: "1.5rem"}}
-                                onClick={navigateToPatientView}>
+                                onClick={OpenWarning2}>
                                 Kvittera
                             </Button>
                         </div>
@@ -82,6 +102,76 @@ const BloodPressurePage = () => {
                 </Modal>
             );
         }
+
+        const ConfirmWarning2 =()=> {
+            return (
+                <Modal
+                    open={openWarningConfirm2}
+                    onClose={handleCloseConfirmation2}
+                    aria-labelledby='modal-popup'
+                >
+                    <div key="modal-popup-div" style={modalStyle} className={classes.paper}>
+                        <NotificationImportant style={{
+                            color: color,
+                            fontSize: '30px',
+                        }}/>
+                        <text className='font-bold mt-2' id='modal-popup'>Uppmärksammat mätvärde</text>
+                        <h2 className='font-bold mt-3' id='modal-popup'>/sätt in datum/ uppmättes vikten /vikt/ av /person/ ?</h2>
+                        <button className='flex shadow'
+                                style={{ border: '2px solid #0066B3', borderRadius: "0px", width: '270px', marginLeft: "auto", marginRight: "auto", marginTop: "1.5rem"}}
+                                onClick={sendToPatient}>
+                            Gå till patientens kontaktuppgifter
+                        </button>
+                        <div className="flex" style={{width: "100%"}}>
+                            <Button
+                                className='flex shadow'
+                                style={{ border: '2px solid #0066B3', borderRadius: "0px", width: '180px', marginLeft: "auto", marginRight: "auto", marginTop: "1.5rem"}}
+                                onClick={OpenWarning3}>
+                                Ta om mätvärde
+                            </Button>
+                            <Button
+                                className='flex shadow'
+                                style={{ border: '2px solid #0066B3', borderRadius: "0px", width: '180px', marginLeft: "auto", marginRight: "auto", marginTop: "1.5rem"}}
+                                onClick={handleCloseConfirmation2}>
+                                Godkänn mätvärde
+                            </Button>
+                        </div>
+                    </div>
+                </Modal>
+            );
+
+        }
+        const ConfirmWarning3 =()=> {
+            return (
+                <Modal
+                    open={openWarningConfirm3}
+                    onClose={handleCloseConfirmation3}
+                    aria-labelledby='modal-popup'
+                >
+                    <div key="modal-popup-div" style={modalStyle} className={classes.paper}>
+                        <NotificationImportant style={{
+                            color: color,
+                            fontSize: '30px',
+                        }}/>
+                        <text className='font-bold mt-2' id='modal-popup'>Uppmärksammat mätvärde</text>
+                        <h2 className='font-bold mt-3' id='modal-popup'>Notis har skickats till patient. Ombedd att ta ett nytt mätvärde</h2>
+                        <div align='right' style={{
+                            marginRight: "10px",
+                            marginBottom: "10px"
+                        }}>
+                            <Button
+                                className='flex shadow'
+                                style={{ border: '2px solid #0066B3', borderRadius: "0px", width: '110px', marginLeft: "auto", marginRight: "auto", marginTop: "1.5rem"}}
+                                onClick={handleCloseConfirmation3}>
+                                Ok
+                            </Button>
+                        </div>
+                    </div>
+                </Modal>
+            );
+
+        }
+
 
 
         return (
@@ -101,6 +191,8 @@ const BloodPressurePage = () => {
 
                 </Link>
                 <ConfirmWarning />
+                <ConfirmWarning2/>
+                <ConfirmWarning3/>
                 </>
 
         )
