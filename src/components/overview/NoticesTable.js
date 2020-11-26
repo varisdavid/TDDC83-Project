18f18ba17f8f473f1b25814078468ffd4f1907af
Emil Strömberg <emist981@student.liu.se>
2020-11-26 10:28:12 +0100

    fixed build warning

--- src/components/overview/NoticesTable.js
+++ src/components/overview/NoticesTable.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
-import { ArrowDropUp, ArrowDropDown } from '@material-ui/icons';
 import { useVirtual } from 'react-virtual';
 //import { RowCells } from '@material-ui/data-grid';
 
