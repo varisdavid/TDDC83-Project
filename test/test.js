f7fe82027798fe224c29913a179414f062b377c9
Gustav Karlsson <gustavkarlsson@Gustavs-MacBook-Pro.local>
2020-11-28 18:09:08 +0100

    CI_test

---
+++ test/test.js
@@ -0,0 +1,7 @@
+describe('Testilitest', function () {
+	it('ladda url', function (){
+	browser.url('./overview/patients')
+	$('/html/body/div/div/div[3]/div/div/div[3]/div/div/div/button[1]').click()
+	new Promise(r=> setTimeout(r,1500));
+	})
+})
