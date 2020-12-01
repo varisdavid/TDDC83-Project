2500731c1374928eb06473c5a499db64af3deef0
Anna Wadsten <annwa479@student.liu.se>
2020-12-02 00:21:33 +0100

    added priorityrules to the database for 3 hospitals

--- backend/backend/blueprints/priorityRule.py
+++ backend/backend/blueprints/priorityRule.py
@@ -9,7 +9,7 @@ from backend.auth import _build_cors_preflight_response, _corsify_actual_respons
 priorityRule_bp = Blueprint("priorityRule", __name__, url_prefix="/priorityRule")
 
 # Route for fetching priority rules for specific operation
-@priorityRule_bp.route("/<integer:ruleID>", methods=["GET", "OPTIONS"])
+@priorityRule_bp.route("/<string:ruleID>", methods=["GET", "OPTIONS"])
 def priorityRules(ruleID):
     if request.method == "OPTIONS":  # CORS preflight
         return _build_cors_preflight_response()
